bd3cb5ba56477ef569814f910ddf672806a3ddf0
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-25 15:21:37 -0300

    feat: toggle between table and form

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -2,13 +2,14 @@ interface BotaoProps {
     cor?: 'green' | 'blue' | 'gray'
     className?: string
     children: any
+    onClick?: () => void
 }
 
 export default function Botao(props: BotaoProps) {
     const cor = props.cor ?? 'gray'
 
     return (
-        <button className={`
+        <button onClick={props.onClick} className={`
             bg-gradient-to-r from-${cor}-600 via-${cor}-500 to-${cor}-600
             text-purple-50 px-4 py-2 rounded-md
             ${props.className}
--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -5,6 +5,8 @@ import Botao from "./Botao";
 
 interface FormularioProps {
     cliente: Cliente
+    clienteMudou?: (cliente: Cliente) => void
+    cancelado?: () => void
 }
 
 export default function Formulario(props: FormularioProps) {
@@ -36,10 +38,10 @@ export default function Formulario(props: FormularioProps) {
             />
 
             <div className="flex justify-end mt-7">
-                <Botao cor="blue" className="mr-2">
+                <Botao cor="blue" className="mr-2" onClick={() => props.clienteMudou?.(new Cliente(nome, +idade, id))}>
                     {id ? 'Aleterar' : 'Salvar'}
                 </Botao>
-                <Botao>
+                <Botao onClick={props.cancelado}>
                     Cancelar
                 </Botao>
             </div>
