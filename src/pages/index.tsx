34035655b83643c7ee3b3fe4f6c96fb04f7a030c
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-24 22:52:49 -0300

    chore: set up

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-tl from-indigo-600 to-green-400
+    `}>
+      <span className={`text-9xl text-purple-200`}>TESTE</span>
+    </div>
+  )
+}
